3ad38c77fffd175a0de3c6082ab5b7ab18cccc69
CristianHuertas <cristian.huertas0@gmail.com>
2021-02-05 15:04:16 -0500

    base de datos auto

--- db/db_conexion.py
+++ db/db_conexion.py
@@ -11,13 +11,13 @@ import psycopg2
 import os
 
 
-#DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 
-SQLALCHEMY_DATABASE_URL = "postgres://lokaqpupicdmkq:31e27bea81a63ef0f4cc16385618377c2c5f280fa1a1ee5d73b5a96a5de6fb3b@ec2-34-230-167-186.compute-1.amazonaws.com:5432/d9t0gdio17cala"
+#SQLALCHEMY_DATABASE_URL = "postgres://lokaqpupicdmkq:31e27bea81a63ef0f4cc16385618377c2c5f280fa1a1ee5d73b5a96a5de6fb3b@ec2-34-230-167-186.compute-1.amazonaws.com:5432/d9t0gdio17cala"
 
-engine                  = create_engine(SQLALCHEMY_DATABASE_URL)
-#engine = create_engine(DATABASE_URL)
+#engine                  = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 
 #Creacion de un creador la Sesion
 SessionLocal = sessionmaker(autocommit=False,
