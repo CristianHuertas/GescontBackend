89fa8492249d09db298fd49d3d35ed3485f7e73a
CristianHuertas <cristian.huertas0@gmail.com>
2021-02-08 13:46:41 -0500

    intentando hacer nuevo endpoint gestiones

--- db/gestion_db.py
+++ db/gestion_db.py
@@ -1,5 +1,5 @@
-from typing import  Dict
-from pydantic import BaseModel
+""" from typing import  Dict
+from pydantic import BaseModel """
 
 
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
