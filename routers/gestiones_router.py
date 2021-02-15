fd8dd03d2f44212118102dc21fd59fdefcfdb9ba
CristianHuertas <cristian.huertas0@gmail.com>
2021-02-14 21:01:52 -0500

    intentando hacer nuevo endpoint gestiones

--- routers/gestiones_router.py
+++ routers/gestiones_router.py
@@ -30,7 +30,7 @@ async def save_gestion(gestion: Gestion, sesion: Session = Depends(obtener_sesio
 
 @router.get("/gestion/registroGetAll/{id_cliente}") #mostrar todos los clientes
 async def get_clientes(id_gestion: int,sesion: Session = Depends(obtener_sesion)):
-    todas_gestiones= sesion.query(GestionInDB).filter_by(id_cliente=id_cliente).all()
+    todas_gestiones= sesion.query(GestionInDB).filter_by(id_cliente=id_gestion).all()
     #todas_gestiones= sesion.query(GestionInDB).order_by(desc(GestionInDB.id_gestion))
 
 
