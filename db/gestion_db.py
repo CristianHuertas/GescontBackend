c19b38dee246d62ab0cc4f9f69af663152dd7089
CristianHuertas <cristian.huertas0@gmail.com>
2021-02-15 13:44:21 -0500

    implementando hora y gestion automaticos

--- db/gestion_db.py
+++ db/gestion_db.py
@@ -16,7 +16,7 @@ class GestionInDB(Base):
 
     id_gestion= Column(Integer, primary_key=True, autoincrement=True)
     id_cliente= Column(Integer)
-    fecha_gestion= Column(DateTime, default=datetime.datetime.utcnow())
+    fecha_gestion= Column(Date, default=datetime.date.utcnow())
     gestor= Column(String)
     tipificacion= Column(String)
     resumen_gestion= Column(String)
