8b6f5ed9a2bd7f5e189fef52aaec2e9da1e142e9
CristianHuertas <cristian.huertas0@gmail.com>
2021-02-14 22:58:43 -0500

    intentando hacer nuevo endpoint gestiones

--- db/gestion_db.py
+++ db/gestion_db.py
@@ -16,7 +16,7 @@ class GestionInDB(Base):
 
     id_gestion= Column(Integer, primary_key=True, autoincrement=True)
     id_cliente= Column(Integer)
-    fecha_gestion= Column(DateTime, default=datetime.datetime.utc-5)
+    fecha_gestion= Column(DateTime, default=datetime.datetime.now)
     gestor= Column(String)
     tipificacion= Column(String)
     resumen_gestion= Column(String)
